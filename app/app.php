f68d0e050aa933dc7246f2baf6b045070d2c3e1b
Jared Beckler <jared.beckler@gmail.com>
2016-03-04 12:23:52 -0800

    add silex routing and twig templating for stores: WIP

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../vendor/Store.php";
-    require_once __DIR__."/../vendor/Brand.php";
+    require_once __DIR__."/../src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
 
     $app = new Silex\Application();
 
@@ -18,6 +18,41 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/stores", function() use ($app) {
+        $stores = Store::getAll();
+
+        return $app['twig']->render('stores.html.twig', array('stores' => $stores));
+    });
+
+    $app->post("/add_store", function() use ($app) {
+        $id = null;
+        $name = $_POST['name'];
+        $new_store = new Store($id, $name);
+        $new_store->save();
+
+        $stores = Store::getAll();
+        return $app['twig']->render('stores.html.twig', array('stores' => $stores));
+    });
+
+    $app->post("/delete_stores", function() use ($app) {
+        Store::deleteAll();
+        $stores = Store::getAll();
+
+        return $app['twig']->render('stores.html.twig', array('stores' => $stores));
+    });
+
+    $app->get("/store/{id}", function($id) use ($app) {
+        $store = Store::find($id);
+        $brands = $store->getBrands();
+        $all_brands = Brand::getAll();
+
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $brands, 'all_brands' => $all_brands));
+    });
 
     return $app;
 ?>
