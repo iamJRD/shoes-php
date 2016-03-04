fafe94ba12241af9816dc4c7b21277ce26dde963
Jared Beckler <jared.beckler@gmail.com>
2016-03-04 08:42:25 -0800

    add passing tests for Store getters

--- src/Store.php
+++ src/Store.php
@@ -1,6 +1,28 @@
 <?php
     class Store
     {
-        
+        private $id;
+        private $name;
+
+        function __construct($id = null, $name)
+        {
+            $this->id = $id;
+            $this->name = $name;
+        }
+
+        function setName($name)
+        {
+            $this->name = $name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
     }
 ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -13,6 +13,32 @@
 
     class StoreTest extends PHPUnit_Framework_TestCase
     {
+        function testGetName()
+        {
+            // Arrange
+            $id = null;
+            $name = "Jared's Shoe Emporium";
+            $test_store = new Store($id, $name);
 
+            // Act
+            $result = $test_store->getName();
+
+            // Assert
+            $this->assertEquals($name, $result);
+        }
+
+        function testGetId()
+        {
+            // Arrange
+            $id = null;
+            $name = "Jared's Shoe Emporium";
+            $test_store = new Store($id, $name);
+
+            // Act
+            $result = $test_store->getId();
+
+            // Assert
+            $this->assertEquals($id, $result);
+        }
     }
 ?>
